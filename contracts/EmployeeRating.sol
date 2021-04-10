3bd6826c8bd60077ec652ee7189a1ec9e257aef7
sweetheart <sweetheart199558@gmail.com>
2021-04-10 04:48:09 -0700

    Added support for multiple skills

--- contracts/EmployeeRating.sol
+++ contracts/EmployeeRating.sol
@@ -4,34 +4,40 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract EmployeeRating {
-    uint public constant MAX_RATING = 5;
-    uint public constant MIN_RATING = 1;
+    uint256 public constant MAX_RATING = 5;
+    uint256 public constant MIN_RATING = 1;
 
     struct Employee{
         string name;
-        mapping (address => mapping(uint => uint)) rating;
+    // TODO: Try cahnging internal maping to array and use .length
+        mapping (address => mapping(uint256 => uint256)) rating;
     }
     mapping (address => Employee) employees;
+    mapping (uint256 => string) skillNames;
 
-    mapping (uint => string) skillNames;
-    uint public constant skillCount = 2;
+    uint256 public skillCount;
+    constructor () {
+        skillNames[0] = "Test Skill 1";
+        skillNames[1] = "Test Skill 2";
+        skillCount = 2;
+    }
 
-    event RatingCast (address indexed from, address indexed to, uint rating );
+    event RatingCast (address indexed from, address indexed to, uint256 rating );
 
 
-    function getRating (address fromAddress, address toAddress) public view returns (uint){
-        uint totalRating;
-        for(uint i; i < skillCount; i++){
+    function getRating (address fromAddress, address toAddress) public view returns (uint256){
+        uint256 totalRating;
+        for(uint256 i; i < skillCount; i++){
             totalRating += employees[toAddress].rating[fromAddress][i];
         }
         return  totalRating/skillCount;
     }
 
-    function rate(address employee, uint[] memory ratings) external {
-        require(ratings.length == skillCount, 'Ratings array should have count equal to skilCount');
+    function rate(address employee, uint256[] memory ratings) external {
+        require(ratings.length == skillCount, 'Ratings array should have count equal to skillCount');
         
-        uint totalRating;
-        for(uint i; i < ratings.length; i++){
+        uint256 totalRating;
+        for(uint256 i; i < ratings.length; i++){
             require(ratings[i] >= MIN_RATING && ratings[i] <= MAX_RATING, 'Rating must be between 1 and 5');
             employees[employee].rating[msg.sender][i] = ratings[i];
             totalRating = totalRating + ratings[i];
@@ -40,7 +46,11 @@ contract EmployeeRating {
         emit RatingCast (msg.sender, employee, totalRating/ratings.length);
     }
 
-    function getSkillName (uint skillIndex) external view returns (string memory){
+    function getSkillName(uint256 skillIndex) external view returns (string memory){
         return skillNames[skillIndex];
+    }
+
+    function getSkillCount() public view returns (uint256){
+        return skillCount;
     } 
 }
--- test/Rating.ts
+++ test/Rating.ts
@@ -5,40 +5,50 @@ const { assert, expect } = require("chai");
 
 describe("Unit tests", function () {
   describe("Rating an employee", () => {
-    let hardhatToken: Contract; 
+    let ratingCountract: Contract; 
     before( async () => {
       const EmployeeRating = await ethers.getContractFactory("EmployeeRating");
-      hardhatToken = await EmployeeRating.deploy();
+      ratingCountract = await EmployeeRating.deploy();
     });
     
     it("Should have inital rating of 0", async () => {
       const [owner, addr1] = await ethers.getSigners();
       const ratings = [];
-      ratings.push(await hardhatToken.getRating(owner.address, owner.address));
-      ratings.push(await hardhatToken.getRating(owner.address, addr1.address));
+      ratings.push(await ratingCountract.getRating(owner.address, owner.address));
+      ratings.push(await ratingCountract.getRating(owner.address, addr1.address));
       assert.equal(ratings[0], 0);
       assert.equal(ratings[1], 0);
     });
 
     it("Should have updated rating after casting Rating", async () => {
       const [owner, addr1] = await ethers.getSigners();
-      await hardhatToken.rate(addr1.address, [1, 5])
-      const aliceRating = await hardhatToken.getRating(owner.address, addr1.address);
+      await ratingCountract.rate(addr1.address, [1, 5])
+      const aliceRating = await ratingCountract.getRating(owner.address, addr1.address);
       assert.equal(aliceRating, 3);
     });
 
     it("Should reject a 0 rating", async () => {
       const [addr1] = await ethers.getSigners();
       await expect(
-      hardhatToken.rate(addr1.address, [0,0])
+      ratingCountract.rate(addr1.address, [0,0])
       ).to.be.revertedWith('Rating must be between 1 and 5');
     });
 
     it("Should reject a 6 rating", async () => {
       const [addr1] = await ethers.getSigners();
       await expect(
-        hardhatToken.rate(addr1.address, [6,6])
+        ratingCountract.rate(addr1.address, [6,6])
         ).to.be.revertedWith('Rating must be between 1 and 5');
+    });
+
+    it("Should return first skill name string", async () => {
+      const firstSkillName = await ratingCountract.getSkillName(0);
+      assert.equal(firstSkillName, 'Test Skill 1');
+    });
+
+    it("Should return skillCount Integer", async () => {
+      const skillCount = await ratingCountract.getSkillCount();
+      assert.equal(skillCount, 2);
     });
   });
 });
