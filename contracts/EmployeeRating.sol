73262aae9704fd221b25c69aab77be76a1c2b0ce
sweetheart <sweetheart199558@gmail.com>
2021-04-10 04:04:03 -0700

    Fixed support for multiple skills

--- contracts/EmployeeRating.sol
+++ contracts/EmployeeRating.sol
@@ -4,43 +4,53 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract EmployeeRating {
-    uint public constant MAX_RATING = 5;
-    uint public constant MIN_RATING = 1;
+    uint256 public constant MAX_RATING = 5;
+    uint256 public constant MIN_RATING = 1;
 
     struct Employee{
         string name;
-        mapping (address => mapping(uint => uint)) rating;
+        mapping (address => uint256[]) rating;
     }
     mapping (address => Employee) employees;
+    string[] skillNames;
 
-    mapping (uint => string) skillNames;
-    uint public constant skillCount = 2;
-
-    event RatingCast (address indexed from, address indexed to, uint rating );
+    uint256 public skillCount = 2;
 
 
-    function getRating (address fromAddress, address toAddress) public view returns (uint){
-        uint totalRating;
-        for(uint i; i < skillCount; i++){
+    constructor () {
+        skillNames[0] = "Test Skill 1";
+        skillNames[1] = "Test Skill 2";
+    }
+
+    event RatingCast (address indexed from, address indexed to, uint256 rating );
+
+
+    function getRating (address fromAddress, address toAddress) public view returns (uint256){
+        uint256 totalRating;
+        for(uint256 i; i < skillCount; i++){
             totalRating += employees[toAddress].rating[fromAddress][i];
         }
         return  totalRating/skillCount;
     }
 
-    function rate(address employee, uint[] memory ratings) external {
+    function rate(address employee, uint256[] memory ratings) external {
         require(ratings.length == skillCount, 'Ratings array should have count equal to skilCount');
         
-        uint totalRating;
-        for(uint i; i < ratings.length; i++){
+        uint256 totalRating;
+        for(uint256 i; i < ratings.length; i++){
             require(ratings[i] >= MIN_RATING && ratings[i] <= MAX_RATING, 'Rating must be between 1 and 5');
             employees[employee].rating[msg.sender][i] = ratings[i];
-            totalRating = totalRating + ratings[i];
+            totalRating +=  ratings[i];
         }
         
         emit RatingCast (msg.sender, employee, totalRating/ratings.length);
     }
 
-    function getSkillName (uint skillIndex) external view returns (string memory){
-        return skillNames[skillIndex];
+    function getSkillNames() external view returns (string[] memory){
+        return skillNames;
+    }
+
+    function getSkillCount() public view returns (uint256){
+        return skillNames.length;
     } 
 }
